cd1ac31a2280a6217d06fe7091c67f8553157024
ConanHUN <conan_513@hotmail.com>
2013-08-29 22:08:03 +0200

    add map build

--- build.bat
+++ build.bat
@@ -20,7 +20,7 @@ echo.
 ..\Tools\DSCreateKey.exe dayz_code.pbo
 ..\Tools\DSCreateKey.exe dayz_epoch.pbo
 ..\Tools\DSCreateKey.exe dayz_epoch_b.pbo
-..\Tools\DSCreateKey.exe dayz_server.pbo
+REM ..\Tools\DSCreateKey.exe dayz_server.pbo
 ..\Tools\DSCreateKey.exe dayz_sfx.pbo
 ..\Tools\DSCreateKey.exe epoch_buildings.pbo
 
@@ -32,7 +32,7 @@ echo.
 ..\Tools\DSSignFile.exe dayz_code.pbo.biprivatekey dayz_code.pbo
 ..\Tools\DSSignFile.exe dayz_epoch.pbo.biprivatekey dayz_epoch.pbo
 ..\Tools\DSSignFile.exe dayz_epoch_b.pbo.biprivatekey dayz_epoch_b.pbo
-..\Tools\DSSignFile.exe dayz_server.pbo.biprivatekey dayz_server.pbo
+REM ..\Tools\DSSignFile.exe dayz_server.pbo.biprivatekey dayz_server.pbo
 ..\Tools\DSSignFile.exe dayz_sfx.pbo.biprivatekey dayz_sfx.pbo
 ..\Tools\DSSignFile.exe epoch_buildings.pbo.biprivatekey epoch_buildings.pbo
 
@@ -40,13 +40,20 @@ echo.
 echo Move and clean files...
 echo.
 
-move dayz_server.pbo.dayz_server.pbo.bisign ..\Build\Keys\
+move dayz_server.pbo ..\Build\server\
+del *.bikey
 echo.
 move *.pbo ..\Build\@DayZ_Epoch\addons\
 echo.
 move *.bisign ..\Build\@DayZ_Epoch\addons\
 echo.
 del *.biprivatekey
+
+cd .. & cd Server Files\MPMissions\
+..\..\Tools\cpbo.exe -y -p DayZ_Epoch_11.Chernarus
+echo.
+if exist ..\..\Build\MPMissions (RD /S /Q ..\..\Build\MPMissions & mkdir ..\..\Build\MPMissions) else mkdir ..\..\Build\MPMissions\
+move DayZ_Epoch_11.Chernarus.pbo ..\..\Build\MPMissions\
 
 pause
 exit
